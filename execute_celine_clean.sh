7e32d1f2da1731d730c97f8cac0398f055230eda
Celine Kalbermatten <ckalberm@izar1.izar.cluster>
2023-12-09 13:57:24 +0100

    new sh

---
+++ execute_celine_clean.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --chdir /scratch/izar/ckalberm
+#SBATCH --partition=gpu
+#SBATCH --qos=gpu_free
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem 16G
+
+echo STARTING AT `date`
+nvidia-smi
+
+cd /home/ckalberm/EPCE
+echo SUCCESSFULLY CHANGED LOCATION
+
+python3 -u epce.py 
+
+echo FINISHED at `date`
